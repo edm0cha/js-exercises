b7179c9656bb4978d40316813b2ad8d9e45494c5
Edwin Moedano <edwinmoedano@gmail.com>
2021-03-14 14:24:22 -0600

    feat: changes to tests to assert a fail

--- test/duplicate_test_mocha.js
+++ test/duplicate_test_mocha.js
@@ -3,10 +3,9 @@ var duplicates = require("../app/duplicates/index");
 
 describe("Found duplicate number using assert interface from chai module", function(){
     describe("Check if the numbers have some equal", function(){
-        it ("check the returned value is true: assert.isTrue(value)", function(){
+        it("Check the returned value is true: assert.isTrue(value)", function(){
             var result = duplicates.checkDuplicates([1,1])
-            assert.isTrue(result);
-
+            assert.isFalse(result);
         });
-    });  
+    });
 });
